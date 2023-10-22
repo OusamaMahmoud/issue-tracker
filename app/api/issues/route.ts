c6049ec0f7bfd23c1fcd772a3fc7440bd95f505f
Osama Mahmoud <ousama.mah113@gmail.com>
2023-10-22 13:23:44 +0300

    Building an API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const creteIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+export async function POST(requst: NextRequest) {
+  const body = await requst.json();
+  const validation = creteIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
