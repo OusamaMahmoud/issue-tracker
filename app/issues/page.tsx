88f546acdd6409fa2ff5559843bcef5f084d17c8
Osama Mahmoud <ousama.mah113@gmail.com>
2023-10-23 08:23:45 +0300

    Showing issue detail page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import React from "react";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueAction from "./IssueAction";
+import Link from "next/link";
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
   await delay(1000);
@@ -27,7 +27,7 @@ const IssuePage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
