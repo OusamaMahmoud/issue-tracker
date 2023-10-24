0d7ab6aeca0761110657a6c0fd6b7184a000af49
Osama Mahmoud <ousama.mah113@gmail.com>
2023-10-24 14:54:01 +0300

    Show a confirmation dailog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,35 @@
-import { Button } from "@radix-ui/themes";
+'use client'
+import prisma from "@/prisma/client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
-const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color="red">Delete Issue</Button>
+const DeleteIssueButton = async ({ issueId }: { issueId: number }) => {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Confirm Deletion</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Revoke access</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This application will no longer be accessible and any
+          existing sessions will be expired.
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Revoke access
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
