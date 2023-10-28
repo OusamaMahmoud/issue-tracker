244d7903bd5379c792ddcf9da14a8ebd5566a91c
Osama Mahmoud <ousama.mah113@gmail.com>
2023-10-28 21:42:26 +0200

    Building the Barchar

---
+++ app/IssuesBarchart.tsx
@@ -0,0 +1,33 @@
+"use client";
+import {
+  Bar,
+  BarChart,
+  Label,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+interface Props {
+  open: number;
+  inprogress: number;
+  closed: number;
+}
+const IssuesBarchart = ({ open, closed, inprogress }: Props) => {
+  const data = [
+    { label: "Open", value: open },
+    { label: "Closed", value: closed },
+    { label: "In_Progress", value: inprogress },
+  ];
+
+  return (
+    <ResponsiveContainer width={"100%"} height={300}>
+      <BarChart data={data}>
+        <XAxis dataKey={"label"} />
+        <YAxis />
+        <Bar dataKey={"value"} barSize={150} style={{ fill: "var(--accent-9)" }} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default IssuesBarchart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestsIssues from "./LatestsIssues";
+import IssuesBarchart from "./IssuesBarchart";
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -19,5 +20,5 @@ export default async function Home() {
     },
   });
 
-  return <IssueSummary open={open} closed={closed} inprogress={inprogress} />;
+  return <IssuesBarchart open={open} closed={closed} inprogress={inprogress} />;
 }
